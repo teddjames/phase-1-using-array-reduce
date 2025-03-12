749884e13d03860e99291306e7437b5c95e03c36
Tedd James <tedd.james@student.moringaschool.com>
2025-03-12 17:16:41 +0300

    Using the Array Reduce Method

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// index.js
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Compute the sum of all batteries using reduce and assign to totalBatteries
+const totalBatteries = batteryBatches.reduce((acc, batch) => acc + batch, 0);
+
+module.exports = { batteryBatches, totalBatteries };
